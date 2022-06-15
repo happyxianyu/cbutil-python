1342bc835358e24d42155c891809ad076946fe29
happyxianyu <happyxianyu623@outlook.com>
2022-06-15 15:22:01 -0400

    add

--- test/test_path.py
+++ test/test_path.py
@@ -30,22 +30,24 @@ def test_move_to():
 
 
 def test_copy_to():
-    test_tmp_path.remove()
-    p1 = test_tmp_path/'1/x'
+    test_path = test_tmp_path/'test_copy_to'
+    test_path.remove()
+    p1 = test_path/'1/x'
     ps1 = [p1/str(v) for v in range(10)]
     for p in ps1:
         p.mkdir()
-    p2 = test_tmp_path/'2/x'
+    p2 = test_path/'2/x'
     p1.copy_to(p2.prnt)
     assert p2.exists()
 
 def test_copy_sons_to():
-    test_tmp_path.remove()
-    p1 = test_tmp_path/'1/x'
+    test_path = test_tmp_path/'test_copy_sons_to'
+    test_path.remove()
+    p1 = test_path/'1/x'
     ps1 = [p1/str(v) for v in range(10)]
     for p in ps1:
         p.mkdir()
-    p2 = test_tmp_path/'2/x'
+    p2 = test_path/'2/x'
     p1.copy_sons_to(p2)
     assert (p2/'1').exists()
 
