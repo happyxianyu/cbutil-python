128ee775bb3be0374a2f96653d78b0aa65b7e8ad
happyxianyu <happyxianyu623@outlook.com>
2023-09-04 18:35:28 -0400

    v1.2.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import *
 
 setup(
     name='cbutil',
-    version='1.2.5',
+    version='1.2.6',
     packages=find_namespace_packages('inc'),
     package_dir={'cbutil': 'inc/cbutil'},
     install_requires=[
